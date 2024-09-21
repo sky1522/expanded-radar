3037463fa4cf80c822251dd1c9ae366e3b2b2a74
sky1522 <42613832+sky1522@users.noreply.github.com>
2024-09-21 09:00:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -485,6 +485,7 @@ function init() {
 
 init();
 
+<!--
 // 마우스 우클릭 방지
 document.addEventListener('contextmenu', function(e) {
   e.preventDefault();
@@ -516,3 +517,4 @@ document.addEventListener('keydown', function(e) {
 document.addEventListener('dragstart', function(e) {
   e.preventDefault();
 }, false);
+-->
