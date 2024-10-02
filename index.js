d880fa38835c82dcf52228120fa11346a346262d
sky1522 <42613832+sky1522@users.noreply.github.com>
2024-10-03 07:41:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const TYPOON1_TIME = 202410021630; //태풍 발표 시각
 
 //태풍현황2
 const TYPOON3_SEQ = 18; //태풍 발생 호
-const TYPOON3_TIME = 202410022200; //태풍 발표 시각
+const TYPOON3_TIME = 202410030400; //태풍 발표 시각
 
 //태풍예측1
 const TYPOON2_NAME = "JEBI"; //태풍 이름
@@ -16,7 +16,7 @@ const TYPOON2_TIME = 2024100206; //태풍 발표 시각
 const TYPOON4_NAME = "KRATHON"; //태풍 이름
 const TYPOON4_NAME1 = "23W"; //태풍 이름
 const TYPOON4_SEQ = 18; //태풍 발생 호
-const TYPOON4_TIME = 2024100206; //태풍 발표 시각
+const TYPOON4_TIME = 2024100212; //태풍 발표 시각
 // 사용자 하드 코딩 영역
 
 const UTC_TIME = 9 * 60 * 60 * 1000;
