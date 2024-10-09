bd3ecbf198a3fb515bf3e15f0962d7914ef8777d
sky1522 <42613832+sky1522@users.noreply.github.com>
2024-10-10 08:48:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 사용자 하드 코딩 영역
 //태풍현황1
 const TYPOON1_SEQ = 19; //태풍 발생 호
-const TYPOON1_TIME = 202410091630; //태풍 발표 시각
+const TYPOON1_TIME = 202410100400; //태풍 발표 시각
 
 //태풍현황2
 const TYPOON3_SEQ = 18; //태풍 발생 호
