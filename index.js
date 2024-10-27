08c1a12bf5384fa928425ad29c8d3d16cea0171a
sky1522 <42613832+sky1522@users.noreply.github.com>
2024-10-28 07:51:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function generateTsUrl() {
     const currentMinute = currentDate.getMinutes();
 
     // 업데이트 시간 설정
-    const updateHours = [4, 7, 10, 16, 22];
+    const updateHours = [4, 10, 16, 22];
 
     // 가장 최근 업데이트 시간을 찾기 위한 로직
     let lastUpdateHour = updateHours.slice().reverse().find(hour =>
