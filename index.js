806909e6928a17692789ee395f94a9775aaa3efc
sky1522 <42613832+sky1522@users.noreply.github.com>
2024-11-26 13:52:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -285,6 +285,9 @@ const baseImages = {
     // 레이더 우박 
     weather23_left_default: `https://afso.kma.go.kr/cgi/rdr/nph-rdr_cmp1_img?tm={T1}&cmp=HAIL&qcd=EXT&obs=ECHO&color=C4&aws=0&acc=&map=D2&grid=2&legend=1&size=495.00&itv=5&zoom_level=2&zoom_x=0000000&zoom_y=0000000&gov=&_DT=`,
     weather23_right_default: `https://afso.kma.go.kr/cgi/rdr/nph-rdr_cmp1_img?tm={T1}&cmp=HAIL&qcd=EXT&obs=ECHO&color=C4&aws=0&acc=&map=HC&grid=2&legend=1&size=495.00&itv=5&zoom_level=2&zoom_x=3350000&zoom_y=5120000&gov=&_DT=`,    
+    // 신적설(일)
+    weather24_left_default: `https://afso.kma.go.kr/cgi/aws3/nph-aws_min_img1?obs=sd_day&tm=202411261345&val=1&stn=1&obj=bn&map=D2&grid=2&legend=1&size=495.00&itv=5&zoom_level=2&zoom_x=0000000&zoom_y=0000000&gov=&_DT=RSW:SDDAY`,
+    weather24_right_default: `https://afso.kma.go.kr/cgi/aws3/nph-aws_min_img1?obs=sd_day&tm=202411261345&val=1&stn=1&obj=bn&map=HC&grid=2&legend=1&size=495.00&itv=5&zoom_level=2&zoom_x=3350000&zoom_y=5120000&gov=&_DT=RSW:SDDAY`,    
 
     //태풍항목
     //태풍현황(1)
@@ -737,7 +740,7 @@ function updateImages(time) {
     }
 
     // 기상 정보 이미지 업데이트
-    if (currentScreenIndex === "weather0" ||currentScreenIndex === "weather1" || currentScreenIndex === "weather2" || currentScreenIndex === "weather3" || currentScreenIndex === "weather4" || currentScreenIndex === "weather5" || currentScreenIndex === "weather6" || currentScreenIndex === "weather7" || currentScreenIndex === "weather8" || currentScreenIndex === "weather9" || currentScreenIndex === "weather10" || currentScreenIndex === "weather11" || currentScreenIndex === "weather12" || currentScreenIndex === "weather13" || currentScreenIndex === "weather14" || currentScreenIndex === "weather15" || currentScreenIndex === "weather16" || currentScreenIndex === "weather17" || currentScreenIndex === "weather18" || currentScreenIndex === "weather19" || currentScreenIndex === "weather20" || currentScreenIndex === "weather21" || currentScreenIndex === "weather22" || currentScreenIndex === "weather23") {
+    if (currentScreenIndex === "weather0" ||currentScreenIndex === "weather1" || currentScreenIndex === "weather2" || currentScreenIndex === "weather3" || currentScreenIndex === "weather4" || currentScreenIndex === "weather5" || currentScreenIndex === "weather6" || currentScreenIndex === "weather7" || currentScreenIndex === "weather8" || currentScreenIndex === "weather9" || currentScreenIndex === "weather10" || currentScreenIndex === "weather11" || currentScreenIndex === "weather12" || currentScreenIndex === "weather13" || currentScreenIndex === "weather14" || currentScreenIndex === "weather15" || currentScreenIndex === "weather16" || currentScreenIndex === "weather17" || currentScreenIndex === "weather18" || currentScreenIndex === "weather19" || currentScreenIndex === "weather20" || currentScreenIndex === "weather21" || currentScreenIndex === "weather22" || currentScreenIndex === "weather23" || currentScreenIndex === "weather24") {
         screen(generateImageURL(time, baseImages[`weather${currentScreenIndex.substr(7)}_left_default`]), generateImageURL(time, baseImages[`weather${currentScreenIndex.substr(7)}_right_default`]));
         currentRightSrc = baseImages[`weather${currentScreenIndex.substr(7)}_right_default`];
     }
